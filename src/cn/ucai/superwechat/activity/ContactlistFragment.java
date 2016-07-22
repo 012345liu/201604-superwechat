37c6b13fa0ff98475cf2dcd05d086be5508a0997
1925293461@qq.com <012345liu>
2016-07-22 18:34:32 +0800

    联系人列表接收广播并更新数据

--- src/cn/ucai/superwechat/activity/ContactlistFragment.java
+++ src/cn/ucai/superwechat/activity/ContactlistFragment.java
@@ -22,8 +22,10 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
@@ -269,6 +271,7 @@ public class ContactlistFragment extends Fragment {
 		} else {
 			progressBar.setVisibility(View.GONE);
 		}
+		updateContactList();
 	}
 
 	@Override
@@ -411,6 +414,7 @@ public class ContactlistFragment extends Fragment {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
 	}
 
 	@Override
@@ -437,6 +441,7 @@ public class ContactlistFragment extends Fragment {
 			} else {
 				progressBar.setVisibility(View.GONE);
 			}
+
 		}
 	}
 
@@ -501,5 +506,27 @@ public class ContactlistFragment extends Fragment {
 	    	outState.putBoolean(Constant.ACCOUNT_REMOVED, true);
 	    }
 	    
+	}
+
+	class UpdateContactReceiver extends BroadcastReceiver {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			adapter.notifyDataSetChanged();
+		}
+	}
+
+	UpdateContactReceiver mUpdateContactReceiver;
+
+	private void updateContactList() {
+		mUpdateContactReceiver = new UpdateContactReceiver();
+		IntentFilter filter = new IntentFilter("update_contact_list");
+		getActivity().registerReceiver(mUpdateContactReceiver, filter);
+	}
+
+	@Override
+	public void onDestroyView() {
+		super.onDestroyView();
+		getActivity().unregisterReceiver(mUpdateContactReceiver);
 	}
 }
