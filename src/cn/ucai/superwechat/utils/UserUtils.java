d89155c9d4e28d972489d2fc26c91b78e08b73b9
1925293461@qq.com <012345liu>
2016-07-27 15:22:05 +0800

    显示群组成员的昵称

--- src/cn/ucai/superwechat/utils/UserUtils.java
+++ src/cn/ucai/superwechat/utils/UserUtils.java
@@ -8,11 +8,14 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import java.util.HashMap;
+
 import cn.ucai.superwechat.DemoHXSDKHelper;
 import cn.ucai.superwechat.I;
 import cn.ucai.superwechat.R;
 import cn.ucai.superwechat.SuperWeChatApplication;
 import cn.ucai.superwechat.applib.controller.HXSDKHelper;
+import cn.ucai.superwechat.bean.MemberUserAvatar;
 import cn.ucai.superwechat.bean.UserAvatar;
 import cn.ucai.superwechat.domain.User;
 
@@ -192,6 +195,18 @@ public class UserUtils {
 		return user;
 
 	}
+	public static MemberUserAvatar getAppMemberInfo(String hxid,String username) {
+		MemberUserAvatar member=null;
+		HashMap<String, MemberUserAvatar> members =
+				SuperWeChatApplication.getInstance().getMemberMap().get(hxid);
+		if (members == null || members.size() < 0) {
+			return null;
+		} else {
+			member = members.get(username);
+		}
+		return member;
+
+	}
 
 	public static void setAppCurrentUserNick(Context context,TextView textView) {
 		UserAvatar user = SuperWeChatApplication.getInstance().getUser();
@@ -214,5 +229,14 @@ public class UserUtils {
 		String userName = SuperWeChatApplication.getInstance().getUserName();
 		setAppUserAvatar(context, userName, imageView);
 
+	}
+
+	public static void setAppMemberNick(String hxid, String username, TextView textView) {
+		MemberUserAvatar member = getAppMemberInfo(hxid, username);
+		if (member != null && member.getMUserNick() != null) {
+			textView.setText(member.getMUserNick());
+		} else {
+			textView.setText(username);
+		}
 	}
 }
